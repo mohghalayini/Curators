edafd76e268684871abbcd7aa1430ccfed7c61f9
mohghalayini <mohghalayini90@gmail.com>
2017-10-24 18:02:13 -0400

    test

--- Curators/app/src/main/java/com/example/mohamadghalayini/curators/MainActivity.java
+++ Curators/app/src/main/java/com/example/mohamadghalayini/curators/MainActivity.java
@@ -74,8 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
             case R.id.about: {
-
-
+                
                 break;
             }
 
