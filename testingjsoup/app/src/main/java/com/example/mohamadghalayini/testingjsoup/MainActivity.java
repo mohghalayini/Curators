a6bcb0f990191e904bdb2f0adfa5e71caeb312f7
mohghalayini <mohghalayini90@gmail.com>
2017-10-26 19:56:00 -0400

    this branch contains the website request test.

--- testingjsoup/app/src/main/java/com/example/mohamadghalayini/testingjsoup/MainActivity.java
+++ testingjsoup/app/src/main/java/com/example/mohamadghalayini/testingjsoup/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.mohamadghalayini.testingjsoup;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,12 +15,19 @@ import java.io.IOException;
 ;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView count;
+    TextView rooms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Title().execute();
+
+        count=(TextView) findViewById(R.id.counter);
+        rooms=(TextView) findViewById(R.id.rooms);
+        count.setText("0");
+        firstthread();
+        secondthread();
+        thirdthread();
     }
 
     private class Title extends AsyncTask<Void, Void, Void> {
@@ -46,10 +54,71 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void result) {
             // Set title into TextView
-            Toast.makeText(MainActivity.this, title, Toast.LENGTH_SHORT).show();
+            String c=count.getText().toString();
+            int i=Integer.parseInt(c);
+            c= Integer.toString(++i);
+            count.setText(c);
+            rooms.setText(title);
+
         }
 
     }
+    void firstthread(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                while (true) {
 
+                    new Title().execute();
+                    synchronized (this) {
+                        try {
+                            wait(600);
+                        } catch (Exception e) {
+                        }
+                    }
+                }
+            }
+        }).start();
+
+    }
+    void secondthread(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                while (true) {
+
+                    new Title().execute();
+                    synchronized (this) {
+                        try {
+                            wait(600);
+                        } catch (Exception e) {
+                        }
+                    }
+                }
+            }
+        }).start();
+
+    }
+    void thirdthread(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                while (true) {
+
+                    new Title().execute();
+                    synchronized (this) {
+                        try {
+                            wait(600);
+                        } catch (Exception e) {
+                        }
+                    }
+                }
+            }
+        }).start();
+
+    }
 }
 
