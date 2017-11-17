317e71c9492318d4fa259606a9ae46a657906149
mohghalayini <mohghalayini90@gmail.com>
2017-11-16 20:54:11 -0500

    Admins can now reset counters and change room activity

--- Curators/app/src/main/java/com/example/mohamadghalayini/curators/admindata.java
+++ Curators/app/src/main/java/com/example/mohamadghalayini/curators/admindata.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -25,6 +26,8 @@ import org.jsoup.nodes.Element;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdminData extends AppCompatActivity {
     Context thisthing = this;
@@ -36,6 +39,7 @@ public class AdminData extends AppCompatActivity {
     LinearLayout headerHolder;
     long timeDifference = 0;
     long lastRefresh = 0;
+    Boolean firstTime = true;
     SharedPreferenceHelper statusesAndPreferences;
 
     @Override
@@ -55,6 +59,10 @@ public class AdminData extends AppCompatActivity {
             lastRefresh = System.currentTimeMillis();
             initialiseContainers();
             login();
+        }
+        if (firstTime) {
+            firstTime = false;
+            initialiseListeners();
         }
     }
 
@@ -119,6 +127,7 @@ public class AdminData extends AppCompatActivity {
             String roomCurrentSize;
             String roomCapacity;
             String status;
+            String id;
             for (int i = 0; i < allRooms.length; i++) {
                 String str = allRooms[i];
                 try {
@@ -131,11 +140,14 @@ public class AdminData extends AppCompatActivity {
                     status = str.substring(str.indexOf(":") + 1, str.indexOf(";"));
                     double ratio = ((double) Integer.parseInt(roomCurrentSize)) / ((double) Integer.parseInt(roomCapacity));
                     ratio = Double.parseDouble(new DecimalFormat("##.##").format(ratio * 100));
+                    str = str.substring(str.indexOf(";") + 1, str.length());
+                    id = str.substring(str.indexOf(":") + 1, str.indexOf(";"));
                     ;
                     outputArray.get(0).add(actualRoom);
                     outputArray.get(1).add(roomCurrentSize);
                     outputArray.get(2).add(ratio + "%");
                     outputArray.get(3).add(status);
+                    outputArray.get(4).add(id);
                 } catch (Exception e) {
                 }
             }
@@ -170,17 +182,60 @@ public class AdminData extends AppCompatActivity {
         }
     }
 
+    private class RoomStatusChanger extends AsyncTask<Void, Void, Void> {
+
+        String roomID;
+        String status;
+        String resetter;
+
+        RoomStatusChanger(String roomId, String newStatus) {
+            roomID = roomId;
+            status = newStatus;
+        }
+
+        RoomStatusChanger(String roomId, int NumberCrusher, String oldStatus) {
+            roomID = roomId;
+            resetter = Integer.toString(-NumberCrusher);
+            status = oldStatus;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("title", "");
+                if (resetter != null) {
+                    map.put("counter", resetter);
+                } else map.put("counter", "");
+                map.put("capacity", "");
+                map.put("status", status);
+
+                Document doc = Jsoup.connect("http://gold-hold-183404.appspot.com/rooms/" + roomID + "/edit").data(map).userAgent("Mozilla").post();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            initialiseContainers();
+            login();
+        }
+    }
+
     public void initialiseContainers() {
         new Thread(new Runnable() {
             public void run() {
                 outputArray = new ArrayList<ArrayList<String>>();
-                for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < 5; i++) {
                     outputArray.add(new ArrayList<String>());
                 }
                 outputArray.get(0).add("Room");
                 outputArray.get(1).add("Size");
                 outputArray.get(2).add("Ratio");
                 outputArray.get(3).add("Status");
+                outputArray.get(4).add("Ids");
                 adminListView[0] = (ListView) findViewById(R.id.roomName);
                 adminListView[1] = (ListView) findViewById(R.id.roomSize);
                 adminListView[2] = (ListView) findViewById(R.id.roomOcc);
@@ -220,12 +275,69 @@ public class AdminData extends AppCompatActivity {
                             lastRefresh = System.currentTimeMillis();
                             initialiseContainers();
                             new RoomFetcher().execute();
+                            initialiseListeners();
                         }
                         refresher.setRefreshing(false);
                     }
                 }
         );
     }
+
+    public void roomStatus(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(AdminData.this);
+        builder.setPositiveButton("Active", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                new RoomStatusChanger(outputArray.get(4).get(position), "Active").execute();
+            }
+        });
+        builder.setTitle("Room:" + outputArray.get(0).get(position) + " Status");
+        builder.setNegativeButton("Inactive", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                new RoomStatusChanger(outputArray.get(4).get(position), "Inactive").execute();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void counterResetter(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(AdminData.this);
+        builder.setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                new RoomStatusChanger(outputArray.get(4).get(position), Integer.parseInt(outputArray.get(1).get(position)), outputArray.get(3).get(position)).execute();
+            }
+        });
+        builder.setTitle(" Reset counter for Room:" + outputArray.get(0).get(position) + "?");
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void initialiseListeners() {
+        adminListView[3].setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                if (position != 0) {
+                    roomStatus(position);
+                }
+            }
+        });
+        adminListView[1].setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                if (position != 0) {
+                    counterResetter(position);
+                }
+            }
+        });
+    }
+
+
 }
 
 
